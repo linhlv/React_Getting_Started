4d1ef8728ea1abfa450526388beea441663bfc68
Linh Le <linh.le@smoovapp.com>
2018-06-06 15:30:04 +0700

    Function and Class Component

--- src/App.js
+++ src/App.js
@@ -26,8 +26,14 @@ class Button extends Component{
   
   handleClick = ()=>{
   	// this === component instance
-    this.setState({
-    	counter: this.state.counter+1
+    // this.setState({
+    // 	counter: this.state.counter+1
+    // })
+
+    this.setState((prevState)=>{
+      return {
+        counter: prevState.counter + 1
+      }
     })
   }
   
