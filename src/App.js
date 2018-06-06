5f8f96c39abf0ce521e289b3cacf38f3d608f9ec
Linh Le <linh.le@smoovapp.com>
2018-06-06 15:28:31 +0700

    Function and Class Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,51 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/* Function Components
+const Button = (props)=>{
+	return (
+  	<button>Go</button>
+  );
+};
+*/
+
+/* Class Components */
+class Button extends Component{
+  /*
+  constructor(props){
+		super(props);
+    this.state = {
+    	counter : 9
+    }
+  }
+  */
+  state = {
+    	counter : 9
+  }
+  
+  handleClick = ()=>{
+  	// this === component instance
+    this.setState({
+    	counter: this.state.counter+1
+    })
+  }
+  
+	render(){
+  	return (
+    	<button onClick={this.handleClick}>{this.state.counter}</button>
+    );
+  };
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header">          
+          <h1 className="App-title">Welcome</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Button></Button>
         </p>
       </div>
     );
