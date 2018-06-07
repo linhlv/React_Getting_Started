1839329836c0911e76afc8e51b1c0cb25a01481d
Linh Le <linh.le@smoovapp.com>
2018-06-07 10:43:35 +0700

    UI and data step for Building the Game Interface

--- src/App.js
+++ src/App.js
@@ -1,66 +1,80 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-const Card = (props)=>{
+const Stars = (props)=>{
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
+
+  let stars = [];
+
   return (
-    <div className="info">
-      <img src={props.avatar_url}/>
-      <div>
-        <div>{props.name}</div>
-        <div>{props.company}</div>
-      </div>
+    <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      {_.range(numberOfStars).map(i=>
+        <i key={i} className="fa fa-star"></i>
+      )}
     </div>
   );
-};
+}
 
-const CardList = (props)=>{
+const Button = (props) =>{
   return (
-    <div>
-      {props.cards.map(card =><Card key={card.toString() + new Date().getMilliseconds().toString()} {...card} />)}      
+    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props)=>{
+  return (
+    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+      <span>5</span>
+      <span>6</span>
+    </div>
+  );
+}
+
+const Numbers = (props)=>{
+  return (
+    <div className="card text-center">
+      <div>
+        {Numbers.list.map((number,i)=>
+          <span key={i}>{number}</span>
+        )}
+        <span>1</span>
+        <span className="selected">2</span>
+        <span className="used">3</span>
+      </div>
     </div>
   )
 }
 
-class Form extends Component {
-  handleSubmit = (event)=>{    
-    event.preventDefault();
-    console.log(this.userNameInput.value)
-  };
+Numbers.list = _.range(1, 10);
 
+class Game extends Component{
   render(){
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" 
-          ref={(input) => this.userNameInput = input}
-          placeholder="Github Username" required/>
-        <button type="submit">Add card</button>
-      </form>
+      <div className="container-fluid">     
+        <h3>Play Nice</h3>
+        <hr/>
+        <div className="row">
+          <Stars/>
+          <Button/>
+          <Answer/>
+        </div>
+        <br/>
+        <Numbers/>
+      </div>
     );
-  };
+  }
 }
 
 class App extends Component {
-  state = {
-    cards: [
-      {
-        name: 'Paul O\'Shannesy',
-        avatar_url : 'https://avatars.githubusercontent.com/u/8445?v=3',
-        company: 'Facebook'
-      },
-      {
-        name: 'Ben Alpert',
-        avatar_url : 'https://avatars.githubusercontent.com/u/6820?v=3',
-        company: 'Facebook'
-      }
-    ]    
-  };
-
   render(){
     return(
       <div>
-        <Form/>
-        <CardList cards={this.state.cards}/>
+        <Game/>       
       </div>
     );    
   };
